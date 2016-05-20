d4e2c69e8206031efd9885f1a306df8b22c1309a
ZoroC4 <grylls@gaojihuanleduozhu.local>
2016-05-20 22:20:39 +0800

    Initial Commit

---
+++ Girlfriend/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Girlfriend
+//
+//  Created by grylls on 16/5/20.
+//  Copyright © 2016年 grylls. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
